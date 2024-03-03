bd74e91c124e4c85347f66f8b17f754f7cc3fbb4
biswasbittu <biswasbittu7415@yahoo.com>
2024-03-03 21:41:18 +0530

    pragraph slice done

--- src/components/singleCard/SingleCards.jsx
+++ src/components/singleCard/SingleCards.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SingleCards = (props) => {
     console.log(props.singleData);
-    const { strMealThumb } = props.singleData;
+    const { strMealThumb, strMeal, strInstructions } = props.singleData;
+    const [paragraphlength, setParagraphlength] = useState(false);
+
+    const paragraph = strInstructions.slice(0, paragraphlength ? 500 : 100);
+    const handlePragraph=()=>{
+      setParagraphlength(true)
+    }
     return (
       <div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 lg:w-full bg-base-100 shadow-xl">
           <div className="p-4 ">
             <figure className="rounded-lg">
               <img src={strMealThumb} alt="Shoes" />
@@ -13,10 +19,16 @@ const SingleCards = (props) => {
           </div>
           <div className="card-body">
             <h2 className="card-title">
-              Shoes!
+              {strMeal}
               <div className="badge badge-secondary">NEW</div>
             </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <p className="leading-2">
+              {" "}
+              {paragraph}{" "}
+              {!paragraphlength&&<span className="cursor-pointer " onClick={handlePragraph} >.......</span>}
+            </p>
+
+            {/* {strInstructions.slice(0,6)} */}
             <div className="card-actions justify-end">
               <div className="badge badge-outline">Fashion</div>
               <div className="badge badge-outline">Products</div>
